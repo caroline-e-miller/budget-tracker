092488962438bbffb0fb66637ee4e23b07398fae
caroline-e-miller <caroline.miller@ymail.com>
2021-05-07 10:39:48 -0500

    linked manifest, moved service worker embed

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js").then(reg => {
+      console.log("Found it!", reg);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
 
@@ -13,14 +21,6 @@ fetch("/api/transaction")
     populateTable();
     populateChart();
   });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js").then(reg => {
-      console.log("Found it!", reg);
-    });
-  });
-}
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
